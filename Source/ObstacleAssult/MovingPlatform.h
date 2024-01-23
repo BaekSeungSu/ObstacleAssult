9dede479ebbcba5e200fb06d005096755337b67c
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-01-24 01:31:34 +0900

    UPROPERTY Variable

--- Source/ObstacleAssult/MovingPlatform.h
+++ Source/ObstacleAssult/MovingPlatform.h
@@ -23,4 +23,13 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 99;
+
+	UPROPERTY(EditAnywhere)
+	float MyFloat = 0.1;
+	
+	UPROPERTY(EditAnywhere)
+	bool MyBool = true;
+
 };
